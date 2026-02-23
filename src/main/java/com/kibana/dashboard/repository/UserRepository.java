51a6b133c081dad78cfd1fbcf0458d5b0b96a56c
matthew-sungwoo <kimjomer@gmail.com>
2026-02-23 22:01:31 +0900

    Creating UserRepository interface

---
+++ src/main/java/com/kibana/dashboard/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.kibana.dashboard.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.kibana.dashboard.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    // Custom query methods can be defined here
+}
